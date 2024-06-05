f238b69e3d880832e2c439453fe8c672c77cea90
IslamTamjid <towhidulislam24@protonmail.com>
2024-06-06 03:07:25 +0600

    use oif evaluator expression

--- React-Basic/src/main.jsx
+++ React-Basic/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+const now = 'Now!'
 const reactElement = React.createElement(
     'a',
     {
       href: 'www.google.com',
       target: '_blank'
     },
-    'Visit Google'
+    'Visit Google',
+    now
 )
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
