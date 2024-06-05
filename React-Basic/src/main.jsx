91614fc587418624395cd0195244efc6c4310a11
IslamTamjid <towhidulislam24@protonmail.com>
2024-06-06 03:02:17 +0600

    creating react elements through React built in method

--- React-Basic/src/main.jsx
+++ React-Basic/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+const reactElement = React.createElement(
+    'a',
+    {
+      href: 'www.google.com',
+      target: '_blank'
+    },
+    'Visit Google'
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  reactElement
 )
